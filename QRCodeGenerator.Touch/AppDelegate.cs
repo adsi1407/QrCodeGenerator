95d3f2fdc2c6130d35441e42dd50709bb6fa6681
David Santacoloma <adsi1407@gmail.com>
2019-01-31 11:31:29 -0500

    Agregar prueba de concepto en iOS

---
+++ QRCodeGenerator.Touch/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using UIKit;
+
+namespace QRCodeGenerator.Touch
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var mainController = new MainViewController();
+            Window.RootViewController = new UINavigationController(mainController);
+            Window.MakeKeyAndVisible();
+            return true;
+        }
+    }
+}
+
---
+++ QRCodeGenerator.Touch/MainViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+using ZXing.Mobile;
+
+namespace QRCodeGenerator.Touch
+{
+    public class MainViewController: UIViewController
+    {
+        private UITextField textFieldTextToCode;
+        private UIButton buttonGenerateCode;
+        private UIImageView imageViewQrCode;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+            Title = "QrCodeGenerator";
+            SetupViews();
+        }
+
+        private void SetupViews()
+        {
+            textFieldTextToCode = new UITextField();
+            textFieldTextToCode.Frame = new CGRect(10, 100, 300, 44);
+            textFieldTextToCode.BackgroundColor = UIColor.LightGray;
+
+            View.AddSubview(textFieldTextToCode);
+
+            buttonGenerateCode = UIButton.FromType(UIButtonType.System);
+            buttonGenerateCode.Frame = new CGRect(10, 180, 300, 44);
+            buttonGenerateCode.SetTitle("Generate", UIControlState.Normal);
+            buttonGenerateCode.TouchUpInside += ButtonGenerateCode_TouchUpInside;;
+            View.AddSubview(buttonGenerateCode);
+
+            imageViewQrCode = new UIImageView();
+            imageViewQrCode.Frame = new CGRect(10, 240, 60, 60);
+            View.AddSubview(imageViewQrCode);
+        }
+
+        private void ButtonGenerateCode_TouchUpInside(object sender, EventArgs e)
+        {
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = ZXing.BarcodeFormat.QR_CODE;
+            barcodeWriter.Options = new ZXing.Common.EncodingOptions
+            {
+                Margin = 1
+            };
+
+            UIImage qrImage = barcodeWriter.Write(textFieldTextToCode.Text);
+            imageViewQrCode.Image = qrImage;
+        }
+
+    }
+}
